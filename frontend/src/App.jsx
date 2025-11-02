de71ef32e3d8e9307879b03c4c8f6306517f0187
Dagmawit43 <dagmawitgebreweld43@gmail.com>
2025-11-02 17:28:11 +0300

    Add frontend with Vite + React + Tailwind setup and basic App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div className="text-[50px] text-center mt-10 font-bold">
+      Internship Tracking System
+    </div>
+         
+      
+    </>
+  )
+}
+
+export default App
